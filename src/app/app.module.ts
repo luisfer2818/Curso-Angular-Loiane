bd4e8b2b8a2589c3b718da5649900a0ceb30e056
luisfer2818 <luisfernando1011@hotmail.com>
2023-06-01 15:53:26 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro-component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/meu-primeiro/meu-primeiro-component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro-component',
+  template: ` <p>meu primeiro componente com Angular</p>`,
+})
+export class MeuPrimeiroComponent {}
